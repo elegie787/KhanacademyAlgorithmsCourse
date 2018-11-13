d1b8108415131d126a1fbd9aef6777052bc4f2e4
Nadine Beckwith <nadinenh787@gmail.com>
2018-11-13 10:51:02 -0600

    Fixing guessing game to make the computer do the work... duh nadine

--- guessingGame.rb
+++ guessingGame.rb
@@ -2,25 +2,24 @@ min = 1
 puts "Enter the maximum number:"
 max = gets
 puts "I will guess a number from 1 to #{max}"
-answer = rand(max.to_i)
-guess = 0
-puts answer.to_s
-puts "Please guess a number"
-guess = gets
-puts guess.to_s
+answer = rand 1..max.to_i
+puts answer
+guess = ((min.to_i + max.to_i) / 2).floor
+puts "Your first guess was #{guess}"
 while guess.to_i != answer.to_i
-
   if guess.to_i == answer.to_i
     break
   elsif guess.to_i > answer.to_i
     max = guess.to_i - 1
-    puts "You are too high, please select a number between #{min} and #{max}"
-    guess = gets
+    puts "Your guess of #{guess} was too high"
+    guess = ((min.to_i + max.to_i) / 2).floor
+  
   else 
     min = guess.to_i + 1
-    puts "You are too low, please select a number between #{min} and #{max}"
-    guess = gets
+    puts "Your Guess of #{guess} was too low"
+    guess = ((min.to_i + max.to_i) / 2).floor
+
   end
 end
 
-puts "You Win!"
+puts "You Win! the correct answer was #{guess}"
